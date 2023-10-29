8e22a68370560327f45328e90361764ba0575dbc
olashile <shilebalogun0507@gmail.com>
2023-10-29 11:07:33 +0100

    calculator

--- function.js
+++ function.js
@@ -4,14 +4,14 @@ let currentValue= '';
 function appendToDisplay(value) {
   currentValue += value;
   display.value = currentValue;
-}
+};
 
 function clearDisplay() {
   currentValue = '';
   display.value = '';
-}
+};
 
-function calculatorResult() {
+function calculateResult() {
   try{
     currentValue = eval(currentValue);
     display.value = currentValue;
@@ -20,3 +20,13 @@ function calculatorResult() {
     currentValue = '';
   }
 }
+
+// (calculateResult) =>{
+//   try{
+//     currentValue = eval(currentValue);
+//     display.value = currentValue;
+//   } catch(error){
+//     display.value = "Error";
+//     currentValue = '';
+//   }
+// }
